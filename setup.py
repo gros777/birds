1fccc96f8954c0180799a3b600eb10cf350fbc9f
Victor A. Hernández <gros777@gmail.com>
2014-07-26 18:52:27 -0700

    Project structure generated with newPythonProject script

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'birds',
+    'author': 'Victor A. Hernandez',
+    'url': 'birds.victorhernandez.me.',
+    'download_url': 'birds.victorhernandez.me/download',
+    'author_email': 'mail@victorhernandez.me.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['birds'],
+    'scripts': [],
+    'name': 'birds'
+}
+
+setup(**config)
