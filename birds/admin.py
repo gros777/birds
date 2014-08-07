a675c4290472e3d2f4cfc4ee877d02f6940fd897
Victor A. Hernández <gros777@gmail.com>
2014-08-07 15:04:08 -0700

    Finalized first sprint for the birds admin

--- birds/admin.py
+++ birds/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
 from birds.models import *
-# Register your models here.
-admin.site.register(Poll)
-admin.site.register(Choice)
-admin.site.register(SuperiorTaxon)
-admin.site.register(Specimen)
-admin.site.register(Variety)
+
+class VarietyInline(admin.StackedInline):
+    model = Variety
+    extra = 1
+
+class VarietyImagesInline(admin.TabularInline):
+    model = VarietyImages
+
+class SuperiorTaxonAdmin(admin.ModelAdmin):
+    fields = ['reino', 'philum', 't_class']
+    list_display = ('reino', 'philum', 't_class')
+
+class SpecimenAdmin(admin.ModelAdmin):
+    fields = ['superior_taxon','order', 'family',
+              'genre', 'species', 
+              'common_name', 'bibliography']
+    list_display = ('common_name', 
+                    'scientific_name', 
+                    'family', 
+                    'genre', 
+                    'species')
+    list_filter = ['family', 'genre']
+    search_fields = ['common_name', 
+                     'family', 
+                     'species']
+
+class VarietyAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None,               {'fields' : ['specimen', 'description']}),
+        ('Sight Information', {'fields' : ['sighting_place', 'sighting_date']})
+    ]
+    list_display = ('description', 
+                    'sighting_place',
+                    'sighting_date')
+    list_filter = ['sighting_date', 
+                   'sighting_place']
+    search_fields = ['description', 
+                    'sighting_place',]
+
+    inlines = [VarietyImagesInline]
+
+# Register your models here
+admin.site.register(SuperiorTaxon, SuperiorTaxonAdmin)
+admin.site.register(Specimen, SpecimenAdmin)
+admin.site.register(Variety, VarietyAdmin)
--- birds/models.py
+++ birds/models.py
@@ -2,56 +2,39 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-
 # Create your models here.
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):  # Python 3: def __str__(self):
-        return self.question
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):  # Python 3: def __str__(self):
-        return self.choice_text
-
-
 class SuperiorTaxon(models.Model):
-    reino = models.CharField(max_length=200)
-    philum = models.CharField(max_length=200)
-    t_class = models.CharField(max_length=200)
+    reino = models.CharField(max_length=200, verbose_name='Kingdom')
+    philum = models.CharField(max_length=200, verbose_name='Phylum')
+    t_class = models.CharField(max_length=200, verbose_name='Class')
     
     def __str__(self):
         return "SuperiorTaxon(Reign: '%s', Philum: '%s', Class = '%s')" % (
         self.reino, self.philum, self.t_class)
 
 class Specimen(models.Model):
-    superior_taxon = models.ForeignKey(SuperiorTaxon)
+    superior_taxon = models.ForeignKey(SuperiorTaxon, verbose_name='Higer classification')
     order = models.CharField(max_length=200)
     family = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200, verbose_name='Genus')
     species = models.CharField(max_length=200)
     common_name = models.CharField(max_length=200)
     bibliography = models.CharField(max_length=200)
+
+    def scientific_name(self):
+        return self.genre + ' ' + self.species
     
     def __str__(self):
         return  ("Specimen(Scientific Name: %s, Common name: %s)" % (
             self.genre + " " + self.species, self.common_name))
 
 class Variety(models.Model):
-    description = models.CharField(max_length=200)
-    sighting_place = models.CharField(max_length=200)
-    sighting_date = models.DateTimeField('date of sight')
+    description = models.CharField(max_length=200, verbose_name='Description')
+    sighting_place = models.CharField(max_length=200, verbose_name='Place of Sight')
+    sighting_date = models.DateTimeField('Date of Sight')
     specimen = models.ForeignKey(Specimen)
-    image = models.ImageField(upload_to='images')
+    
     # bynary field management
     # _image = models.CharField(db_column='image',
     #         blank=True)
@@ -67,4 +50,8 @@ class Variety(models.Model):
         return ("Variety(%s, Sight place: %s)" % (
                 self.description, self.sighting_place))
 
-
+class VarietyImages(models.Model):
+    variety = models.ForeignKey(Variety)
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
